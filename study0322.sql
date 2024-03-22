2771b502823cadeb56302e2ca170309b8bedb978
wock5 <162662483+wock5@users.noreply.github.com>
2024-03-22 12:56:13 +0900

    Add files via upload

---
+++ study0322.sql
@@ -0,0 +1,43 @@
+select ename, sal*12+nvl(comm,0)
+from emp
+/
+select ename, sal*12
+from emp
+/
+select ename, sal*12+nvl(comm,0)
+from emp
+/
+select ename, sal*12
+from emp
+/
+select ename, sal*12+nvl(comm,0) AS ¿¬ºÀ
+from emp
+/
+select * from emp
+/
+select * from dept
+/
+select * from emp
+/
+select * from dept
+/
+select * from emp
+/
+select * from dept
+/
+select* from dept
+/
+select* from dept
+/
+select* from emp
+/
+select* from emp
+/
+select* from emp
+/
+select * from emp
+/
+select * from emp
+/
+select * from emp
+/
